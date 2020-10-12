c25554927756d077d35c19e0b636f925a9f43b25
Vincent He <vincent0710@Outlook.com>
2020-10-12 17:00:53 -0400

    Using Picker And For Each Make picker looks better

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,18 +10,36 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkout = ""
     @State private var tipindex = 2
-    @State private var numofpeople = 2
+    @State private var numofpeople = 0
     
-    var tipoptions = [10,15, 20, 25, 0]
+    var tipoptions = [10,15, 20, 25]
     var body: some View {
-        Form{
-            Section{
-                TextField("Enter Amount: ", text: $checkout).keyboardType(.decimalPad) // Not using the full keyboard
-            }
-            Section{
-                Text("Checkout Amount: \(checkout)")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter Amount: ", text: $checkout).keyboardType(.decimalPad) // Not using the full keyboard
+                    
+                    Picker("Number of people", selection: $numofpeople){ // This is a Index
+                        ForEach(2..<100){ number in
+                            Text("\(number)")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Pick Percent", selection: $tipindex){
+                        ForEach(0 ..< tipoptions.count ){ num in
+                            Text("Tip: \(self.tipoptions[num]) ") // Convert
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle()) // A Better looking Picker
+                }
+                Section{
+                    Text("\(checkout) $")
+                }
             }
         }
+        .navigationBarTitle("We Split")
     }
 }
 
